a0589102e8ef5a9b1e17875004464f380d24b7ff
AimanK <akaya005@fiu.edu>
2023-07-28 18:03:52 -0400

    Add code for Delete, Add, Edit, and Viewing a User

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React, { useState , useEffect } from 'react'
 import axios from "axios"
+import { Link, useParams } from 'react-router-dom';
 
 
 export default function Home() {
 
     const [users, setUsers] = useState ([])
+
+    const {id} = useParams()
 
     useEffect(() => {
         loadUsers();
@@ -15,6 +18,12 @@ export default function Home() {
         const result = await axios.get("http://localhost:8080/users");
         setUsers(result.data);
     }
+
+    const deleteUser=async(id) => {
+        await axios.delete(`http://localhost:8080/user/${id}`)
+        loadUsers()
+    }
+
   return (
     <div className="container">
         <div className="py-4">
@@ -37,10 +46,10 @@ export default function Home() {
                     <td>{user.username}</td>
                     <td>{user.email}</td>
                     <td>
-                        <button className="btn btn-primary mx-2">View</button>
-                        <button className="btn btn-outline-primary mx-2">Edit</button>
-                        <button className="btn btn-danger mx-2">Delete</button>
-                    </td>
+                        <Link to={`/user/${user.id}`} className="btn btn-primary mx-2">View</Link>
+                        <Link to={`/edituser/${user.id}`} className="btn btn-outline-primary mx-2">Edit</Link>
+                        <button className="btn btn-danger mx-2" onClick={() => deleteUser(user.id)}>Delete</button>
+                    </td> 
                     </tr>
                 ))
             }
--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import  { Link ,  useNavigate } from "react-router-dom";
 
 export default function AddUser() {
-  return (
+
+    let navigate = useNavigate();
+
+    const [user, setUser]=useState({
+        name: "",
+        username: "",
+        email: "",
+    });
+
+    const {name, username, email} = user;
+
+    const onInputChange=(e) => {
+        setUser({...user, [e.target.name]: e.target.value });
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post("http://localhost:8080/user", user);
+        navigate("/");
+    };
+
+  return ( 
     <div className="container">
         <div className="row">
             <div className="col-md-6 offset-md-3 border rounded p-4">
                 <h2 clasName="text-center m-4">Register User</h2>
+
+                <form onSubmit={(e) => onSubmit(e)}>
                 <div className="mb-3">
                     <label htmlFor="Name" className="form-label">
                         Name
@@ -15,6 +40,8 @@ export default function AddUser() {
                     className="form-control"
                     placeholder="Enter your name"
                     name="name"
+                    value={name}
+                    onChange={(e)=>onInputChange(e)}
                     />
                 </div>
                 <div className="mb-3">
@@ -24,8 +51,10 @@ export default function AddUser() {
                     <input
                     type={"text"}
                     className="form-control"
-                    placeholder="Enter your name"
-                    name="name"
+                    placeholder="Enter your username"
+                    name="username"
+                    value={username}
+                    onChange={(e)=>onInputChange(e)}
                     />
                 </div>
                 <div className="mb-3">
@@ -35,16 +64,19 @@ export default function AddUser() {
                     <input
                     type={"text"}
                     className="form-control"
-                    placeholder="Enter your name"
-                    name="name"
+                    placeholder="Enter your email"
+                    name="email"
+                    value={email}
+                    onChange={(e)=>onInputChange(e)}
                     />
                 </div>
                 <button type="submit" className="btn btn-outline-primary">
                     Submit
                 </button>
-                <button type="submit" className="btn btn-outline-danger mx-2">
+                <Link className="btn btn-outline-danger mx-2" to="/" >
                     Cancel
-                </button>
+                </Link>
+                </form>
             </div>
         </div>
     </div>
